2b82c0f11faac5c5f75de14220cd9a7018264f4d
Shai James <skyandbeyond@gmail.com>
2018-07-01 00:03:00 +0300

    work

--- setup.sh
+++ setup.sh
@@ -5,3 +5,5 @@ curl -sL https://deb.nodesource.com/setup_8.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh
 sudo apt-get install nodejs -y
 sudo apt-get install build-essential -y
+sudo apt-get install git -y
+git clone https://github.com/nsjames/eos_node_healthchecker.git
